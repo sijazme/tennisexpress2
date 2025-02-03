8cdd37fd42549c0f5b44363060fb8b7fd88d5f79
Ehsan Ijaz <ijazsourcecode@hotmail.com>
2025-02-03 16:17:37 +0500

    ...

--- tennisexpress/routes/index.js
+++ tennisexpress/routes/index.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var router = express.Router();
 
-const service1 = require("./services/TennisService");
+const service1 = require("services/TennisService");
 
 
 /* GET home page. */
